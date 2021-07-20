f313fad9e823869279588c21a7ae619292b2e712
NadejdaSavin <87228745+NadejdaSavin@users.noreply.github.com>
2021-07-20 13:58:20 +0300

    Initial Commit

--- nadejda-savin-react-portfolio/src/components/app.js
+++ nadejda-savin-react-portfolio/src/components/app.js
@@ -1,17 +1,57 @@
 import React, { Component } from "react";
 import moment from "moment";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
-import NavigationContainer from "./navigation/navigation-container"
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/a";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("'/user?ID=12345'")
+      .then(response => {
+        console.log("response data", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    this.getPortfolioItems();
     return (
-      <div className='app'>
-        <NavigationContainer />
-        <h1>Nadejda Savin Portfolio</h1>
-        <div>{moment().format ("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer/>
+      <div className="app">
+        <Router>
+          <div>
+            <h1>Nadejda Savin Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
